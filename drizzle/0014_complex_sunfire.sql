6806a619c53a2b789b70c26690c022198be22ae4
jamesweagum1004 <james.weagum@yandex.com>
2026-08-18 14:46:50 +0900

    Make display locale migration idempotent

--- drizzle/0014_complex_sunfire.sql
+++ drizzle/0014_complex_sunfire.sql
@@ -1,1 +1,1 @@
-ALTER TABLE "titles" ADD COLUMN "display_locales" jsonb DEFAULT '["en","es","fr","de","pt"]'::jsonb NOT NULL;
+ALTER TABLE "titles" ADD COLUMN IF NOT EXISTS "display_locales" jsonb DEFAULT '["en","es","fr","de","pt"]'::jsonb NOT NULL;
